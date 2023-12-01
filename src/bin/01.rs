cc53d303a70868b18368340fe57a7b8f092d34f8
Oliver Tetz <oliver.tetz@tracetronic.de>
2023-12-01 11:31:14 +0100

    solution day 1

---
+++ src/bin/01.rs
@@ -0,0 +1,68 @@
+use std::collections::HashMap;
+advent_of_code::solution!(1);
+
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let lines = input.lines();
+    let mut sum: u32 = 0;
+    for line in lines {
+        let mut vec: Vec<u32> = Vec::new();
+        get_vec_with_numbers(line, &mut vec);
+
+        sum += vec[0] * 10 + vec[vec.len() - 1];
+    }
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let lines = input.lines();
+    let mut sum: u32 = 0;
+    let mut numbers = HashMap::new();
+    let arr = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    for (i, number_word) in arr.iter().enumerate() {
+        numbers.insert(number_word, (i + 1).to_string());
+    }
+
+    for line in lines {
+        let mut vec: Vec<u32> = Vec::new();
+        let mut new_line = String::from(line);
+        let mut change_str: Vec<(usize, &str)> = Vec::new();
+        for number_word in arr {
+            for (match_index, _) in new_line.match_indices(number_word) {
+                change_str.push((match_index, &numbers[&number_word]));
+            }
+        }
+        change_str.sort_by_key(|k| k.0);
+        for (corr, (i, num)) in change_str.iter().enumerate() {
+            new_line.insert_str(*i + corr, num);
+        }
+        get_vec_with_numbers(&new_line, &mut vec);
+        sum += vec[0] * 10 + vec[vec.len() - 1];
+    }
+    Some(sum)
+}
+
+fn get_vec_with_numbers(line: &str, vec: &mut Vec<u32>) {
+    for num in line.chars().map(|s| s.to_digit(10)) {
+        if num.is_some() {
+            vec.push(num.unwrap());
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(142));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples_2", DAY));
+        assert_eq!(result, Some(281));
+    }
+}
